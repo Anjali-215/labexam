03e05e79116798ef3b8f5fb660517cbb7a65a0ae
Anjali-215 <anjalikrishna2163@gmail.com>
2024-08-20 12:15:04 +0530

    submitted

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-
+//modal
         function openModal(img) {
             document.getElementById('modalImage').src = img.src;
             var myModal = new bootstrap.Modal(document.getElementById('imageModal'));
             myModal.show();
         }
 
+//geolocation api
+
+        function initializeMap(lat, lon) {
+            var map = L.map('map').setView([lat, lon], 13);
+
+           //api
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            //  marker
+            L.marker([lat, lon]).addTo(map)
+                .bindPopup('You are here')
+                .openPopup();
+        }
+
+        document.getElementById('showMapButton').addEventListener('click', function() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var lat = position.coords.latitude;
+                    var lon = position.coords.longitude;
+
+                    // Show the map container and initialize the map
+                    document.getElementById('map').style.display = 'block';
+                    initializeMap(lat, lon);
+                }, function() {
+                    alert('Unable to retrieve your location');
+                });
+            } else {
+                alert('Geolocation is not supported by this browser');
+            }
+        });
